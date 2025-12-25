4d49710ee59013d1824fcaa461965e05d749e246
hosanafg <fernandeshosana48@gmail.com>
2025-12-24 23:13:34 -0300

    mod1

--- namespaces.cpp
+++ namespaces.cpp
@@ -6,10 +6,17 @@ using namespace std;
 
 int main () {  
 
-    cout << "Qual o seu nome?:  ";
+    //cout << "Qual o seu nome?:  ";
+    cout << "Qual o seu nome completo?:  ";
     text name;
-    cin >> name;
-    cout << "hello, "<< name;
+    //cin >> name;
+    getline(cin,name);
+
+    int number;
+    cout << "Qual sua idade?:  ";
+    cin >> number;
+
+    cout << "hello, "<< name <<"\nYou're "<< number;
 
     return 0;
 }
