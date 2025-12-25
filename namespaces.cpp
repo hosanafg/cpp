dabef0c808d1fef790e65e6ec1908e3ada078679
hosanafg <fernandeshosana48@gmail.com>
2025-12-24 23:18:45 -0300

    getline

--- namespaces.cpp
+++ namespaces.cpp
@@ -7,16 +7,20 @@ using namespace std;
 int main () {  
 
     //cout << "Qual o seu nome?:  ";
+    cout << "Digite um numero de 1 a 10: ";
+    int numero;
+    cin >> numero;
+
     cout << "Qual o seu nome completo?:  ";
     text name;
     //cin >> name;
-    getline(cin,name);
+    getline(cin>>ws,name);
 
     int number;
     cout << "Qual sua idade?:  ";
     cin >> number;
 
-    cout << "hello, "<< name <<"\nYou're "<< number;
+    cout << numero <<"\n"<< "hello, "<< name <<"\nYou're "<< number;
 
     return 0;
 }
